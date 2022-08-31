6bd26eb9d0490376c3bdeb0189f6069902ef6909
Alex Woods <alexanderwoods2019@gmail.com>
2022-08-31 12:50:56 -0400

    Introducing Serialize Field

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,9 +6,9 @@ public class Mover : MonoBehaviour
 {
     #region Variables
     // Ill do the hungarian notation but im not going to like it.
-    float fltXVal = 0.02f;
-    float fltYVal = 0f;
-    float fltZVal = 0f;
+    [SerializeField] private float fltXVal = 0.02f;
+    [SerializeField] private float fltYVal = 0f;
+    [SerializeField] private float fltZVal = 0f;
     #endregion
 
     #region Unity Methods
